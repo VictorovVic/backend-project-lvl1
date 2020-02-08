222d61cb90fac6280a9a1eb30fae055b22a0ee1b
Viktor Grigorev <7184243@gmail.com>
2020-02-09 00:51:58 +0300

    step 4 - test github action

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import toAskName from '../src/index.js';
+import toAskName, { isEvenNumber } from '../src/index.js';
 
 console.log('Welcome to the Brain Games!');
 toAskName();
+isEvenNumber();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import readlineSync from 'readline-sync';
 
-const toGreet = () => {
-  console.log('Welcome to the Brain Games!');
+const toAskName = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
   const actual = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${actual}!`);
+  return console.log(`Hello, ${actual}!\n`);
 };
 
-export { toGreet as default };
+const getRandom = () => Math.floor(Math.random() * 100);
+
+const isEvenNumber = () => {
+  for (let i = 0; i < 3; i += 1) {
+    console.log(`Question: ${getRandom()}`);
+    readlineSync.question('Your answer: ');
+    console.log('Correct!');
+  }
+  return console.log('Congratulations, SAM!');
+};
+
+export { toAskName as default, isEvenNumber };
